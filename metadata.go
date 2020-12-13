357b154f0c8cf5888b8ca1c7c4e3b38eb106e44a
akeil <alex@akeil.net>
2020-12-13 12:53:31 +0100

    dedicated type for NotebookType

--- metadata.go
+++ metadata.go
@@ -1,78 +1,119 @@
 package rmtool
 
 import (
-    "bytes"
-    "encoding/json"
-    "io/ioutil"
-    "strconv"
-    "time"
-    "fmt"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"time"
 )
 
 // Timestampp is the datatype for a UNIX timestamp in string format.
 type Timestamp struct {
-    time.Time
+	time.Time
 }
+
+type NotebookType int
+
+const (
+	DocumentType = iota
+)
 
 // Metadata holds the metadata for a notebook.
 type Metadata struct {
-	Deleted          bool   `json:"deleted"`
-	LastModified     Timestamp `json:"lastModified"`
-	LastOpenedPage   uint   `json:"lastOpenedPage"`
-	Metadatamodified bool   `json:"metadatamodified"`
-	Modified         bool   `json:"modified"`
-	Parent           string `json:"parent"`
-	Pinned           bool   `json:"bool"`
-	Synced           bool   `json:"synced"`
-	Type             string `json:"type"`
-	Version          uint   `json:"version"`
-	VisibleName      string `json:"visibleName"`
+	Deleted          bool         `json:"deleted"`
+	LastModified     Timestamp    `json:"lastModified"`
+	LastOpenedPage   uint         `json:"lastOpenedPage"`
+	Metadatamodified bool         `json:"metadatamodified"`
+	Modified         bool         `json:"modified"`
+	Parent           string       `json:"parent"`
+	Pinned           bool         `json:"bool"`
+	Synced           bool         `json:"synced"`
+	Type             NotebookType `json:"type"`
+	Version          uint         `json:"version"`
+	VisibleName      string       `json:"visibleName"`
 }
 
 // ReadMetadata reads a Metadata struct from the given JSON file.
 //
 // Note that you can also use `json.Unmarshal(data, m)`.
 func ReadMetadata(path string) (Metadata, error) {
-    var m Metadata
-    data, err := ioutil.ReadFile(path)
-    if err != nil {
-        return m, err
-    }
+	var m Metadata
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return m, err
+	}
 
-    err = json.Unmarshal(data, &m)
-    return m, err
+	err = json.Unmarshal(data, &m)
+	return m, err
 }
 
 func (t *Timestamp) UnmarshalJSON(b []byte) error {
-    // expects a string lke this: 1607462787637
-    // with the last for digits containing nanoseconds.
-    var s string
-    err := json.Unmarshal(b, &s)
-    if err != nil {
-        return err
-    }
+	// expects a string lke this: 1607462787637
+	// with the last for digits containing nanoseconds.
+	var s string
+	err := json.Unmarshal(b, &s)
+	if err != nil {
+		return err
+	}
 
-    n, err := strconv.Atoi(s)
-    if err != nil {
-        return err
-    }
+	n, err := strconv.Atoi(s)
+	if err != nil {
+		return err
+	}
 
-    secs := int64(n / 1_000)
-    nanos := (int64(n) - (secs * 1_000)) * 1_000_000
-    ts := Timestamp{time.Unix(secs, nanos)}
+	secs := int64(n / 1_000)
+	nanos := (int64(n) - (secs * 1_000)) * 1_000_000
+	ts := Timestamp{time.Unix(secs, nanos)}
 
-    *t = ts
-    return nil
+	*t = ts
+	return nil
 }
 
 func (t Timestamp) MarshalJSON() ([]byte, error) {
-    nanos := t.UnixNano()
-    millis := nanos / 1_000_000
+	nanos := t.UnixNano()
+	millis := nanos / 1_000_000
 
-    s := fmt.Sprintf("%d", millis)
-    buf := bytes.NewBufferString(`"`)
+	s := fmt.Sprintf("%d", millis)
+	buf := bytes.NewBufferString(`"`)
 	buf.WriteString(s)
 	buf.WriteString(`"`)
 
-    return buf.Bytes(), nil
+	return buf.Bytes(), nil
+}
+
+func (n *NotebookType) UnmarshalJSON(b []byte) error {
+	var s string
+	err := json.Unmarshal(b, &s)
+	if err != nil {
+		return err
+	}
+
+	var nt NotebookType
+	switch s {
+	case "DocumentType":
+		nt = DocumentType
+	default:
+		return fmt.Errorf("invalid notebook type %q", s)
+	}
+
+	*n = nt
+	return nil
+}
+
+func (n NotebookType) MarshalJSON() ([]byte, error) {
+	var s string
+	switch n {
+	case DocumentType:
+		s = "DocumentType"
+	default:
+		return nil, fmt.Errorf("invalid notebook type %v", n)
+	}
+
+	buf := bytes.NewBufferString(`"`)
+	buf.WriteString(s)
+	buf.WriteString(`"`)
+
+	return buf.Bytes(), nil
 }
--- metadata_test.go
+++ metadata_test.go
@@ -1,48 +1,62 @@
 package rmtool
 
 import (
-    "encoding/json"
+	"encoding/json"
 	"testing"
-    "time"
+	"time"
 )
 
 func TestReadMetadata(t *testing.T) {
-    path := "./testdata/faf24233-a397-409e-8993-914113af7d54.metadata"
-    m, err := ReadMetadata(path)
+	path := "./testdata/faf24233-a397-409e-8993-914113af7d54.metadata"
+	m, err := ReadMetadata(path)
 
-    if err != nil {
+	if err != nil {
 		t.Error(err)
 	}
 
-    if m.Deleted {
-        t.Errorf("unexpected value for deleted")
-    }
-    // TODO: timestamp
+	if m.Deleted {
+		t.Errorf("unexpected value for deleted")
+	}
+	// TODO: timestamp
 
-    if m.Version != 6 {
-        t.Errorf("unexpected value for version")
-    }
+	if m.Version != 6 {
+		t.Errorf("unexpected value for version")
+	}
 
-    if m.VisibleName != "Notebook" {
-        t.Errorf("unexpected value for version")
-    }
+	if m.VisibleName != "Notebook" {
+		t.Errorf("unexpected value for version")
+	}
 
-    t.Errorf("%v", m)
+	// 2020-12-08 22:26:27.637
+	if m.LastModified.Year() != 2020 {
+		t.Errorf("unexpected value for lastModified")
+	}
+	if m.LastModified.Second() != 27 {
+		t.Errorf("unexpected value for lastModified")
+	}
+	if m.LastModified.Nanosecond() != 637_000_000 {
+		t.Errorf("unexpected value for lastModified")
+	}
+
+	if m.Type != DocumentType {
+		t.Errorf("unexpected value for type")
+	}
 }
 
 func TestMarshalMetadata(t *testing.T) {
-    d := time.Date(2020, 12, 13, 7, 23, 43, 589000000, time.UTC)
-    m := Metadata{
-        Deleted: true,
-        LastModified: Timestamp{d},
-        VisibleName: "Test Notebook",
-    }
+	d := time.Date(2020, 12, 13, 7, 23, 43, 589000000, time.UTC)
+	m := Metadata{
+		Deleted:      true,
+		LastModified: Timestamp{d},
+		Type:         DocumentType,
+		VisibleName:  "Test Notebook",
+	}
 
-    data, err := json.Marshal(m)
-    if err != nil {
+	data, err := json.Marshal(m)
+	if err != nil {
 		t.Error(err)
 	}
 
-    s := string(data)
-    t.Errorf(s)
+	s := string(data)
+	t.Errorf(s)
 }
