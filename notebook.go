0bdbfb45ed05a8bb10bb7558bede9e7a8003a0d9
akeil <alex@akeil.net>
2020-12-13 15:33:27 +0100

    Add a data struct to represent the complete Notebook

---
+++ notebook.go
@@ -0,0 +1,72 @@
+package rmtool
+
+import (
+    "fmt"
+    "path/filepath"
+    "io/ioutil"
+)
+
+type Notebook struct {
+    Base string
+    ID string
+    Meta Metadata
+    Content Content
+    Pages []*Page
+}
+
+func NewNotebook(baseDir, id string) *Notebook {
+    return &Notebook{Base: baseDir, ID: id,}
+}
+
+func (n *Notebook) Read() error {
+    mp := filepath.Join(n.Base, n.ID + ".metadata")
+    m, err := ReadMetadata(mp)
+    if err != nil {
+        return err
+    }
+    n.Meta = m
+
+    cp := filepath.Join(n.Base, n.ID + ".content")
+    c, err := ReadContent(cp)
+    if err != nil {
+        return err
+    }
+    n.Content = c
+
+    n.Pages = make([]*Page, len(n.Content.Pages))
+    for i, pageID := range(n.Content.Pages) {
+        p := NewPage(n.Base, n.ID, pageID)
+        n.Pages[i] = p
+    }
+
+    return nil
+}
+
+type Page struct {
+    Base string
+    ID string
+    Meta PageMetadata
+    Drawing *Drawing
+}
+
+func NewPage(baseDir, notebookID, pageID string) *Page {
+    return &Page{
+        Base: filepath.Join(baseDir, notebookID),
+        ID: pageID,
+    }
+}
+
+func (p *Page) ReadDrawing() error {
+    src := filepath.Join(p.Base, p.ID + ".rm")
+    data, err := ioutil.ReadFile(src)
+	if err != nil {
+		return fmt.Errorf("cannot read rm file %q. Error: %v", src, err)
+	}
+    d, err := ReadDrawing(data)
+    if err != nil {
+		return fmt.Errorf("cannot read rm file %q. Error: %v", src, err)
+	}
+
+    p.Drawing = d
+    return nil
+}
