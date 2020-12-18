6f5c5f2e22bbec97aa66d8597929dac69b8db88b
akeil <alex@akeil.net>
2020-12-18 10:01:30 +0100

    Split interface and implementation in separate files

--- storage.go
+++ storage.go
@@ -1,11 +1,5 @@
 package rm
 
-import (
-	"encoding/json"
-	"os"
-	"path/filepath"
-)
-
 type Storage interface {
 	// ReadMetadata reads the metadata for a notebook with the given ID.
 	ReadMetadata(id string) (Metadata, error)
@@ -68,52 +62,4 @@ func ReadFull(s Storage, id string) (*Notebook, error) {
 	}
 
 	return n, nil
-}
-
-type fsStorage struct {
-	Base string
-}
-
-func NewFilesystemStorage(base string) Storage {
-	return &fsStorage{base}
-}
-
-func (f *fsStorage) ReadMetadata(id string) (Metadata, error) {
-	var m Metadata
-	err := readJSON(f.Base, id+".metadata", &m)
-	return m, err
-}
-
-func (f *fsStorage) ReadContent(id string) (Content, error) {
-	var c Content
-	err := readJSON(f.Base, id+".content", &c)
-	return c, err
-}
-
-func (f *fsStorage) ReadDrawing(id, pageId string) (*Drawing, error) {
-	path := filepath.Join(f.Base, id, pageId+".rm")
-	r, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer r.Close()
-
-	return ReadDrawing(r)
-}
-
-func readJSON(base, filename string, dst interface{}) error {
-	path := filepath.Join(base, filename)
-	r, err := os.Open(path)
-	if err != nil {
-		return err
-	}
-	defer r.Close()
-
-	dec := json.NewDecoder(r)
-	err = dec.Decode(dst)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
