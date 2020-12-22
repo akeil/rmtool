3b184560f31f8c96057f9f6f731debef3f49c159
akeil <alex@akeil.net>
2020-12-22 22:23:31 +0100

    Move utility functions to separate file

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -2,7 +2,6 @@ package api
 
 import (
 	"bytes"
-	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -629,48 +628,4 @@ func newRequest(method, base, endpoint, token string, payload interface{}) (*htt
 	//req.Header.Set("Accept", "application/json")  // necessary?
 
 	return req, nil
-}
-
-func resolve(base, endpoint string) (string, error) {
-	b, err := url.Parse(base)
-	if err != nil {
-		return "", err
-	}
-
-	e, err := url.Parse(endpoint)
-	if err != nil {
-		return "", err
-	}
-
-	return b.ResolveReference(e).String(), nil
-}
-
-// parseTokenExpiration retrieves the expiration time from the user token.
-func parseTokenExpiration(token string) (time.Time, error) {
-	var t time.Time
-
-	// split the JWT into its parts (header.payload.signature),
-	// we are only interested in the `payload`.
-	parts := strings.Split(token, ".")
-	if len(parts) < 2 {
-		return t, fmt.Errorf("unexpected number of token segments")
-	}
-
-	// decode from base64
-	data, err := base64.RawURLEncoding.DecodeString(parts[1])
-	if err != nil {
-		return t, err
-	}
-
-	// parse the one field we are interested in
-	jwt := struct {
-		Exp int64 `json:"exp"`
-	}{}
-	dec := json.NewDecoder(bytes.NewReader(data))
-	err = dec.Decode(&jwt)
-	if err != nil {
-		return t, err
-	}
-
-	return time.Unix(jwt.Exp, 0), nil
 }
