8c29ccabc4c2bd45c34c829ce000182d10eca5ee
akeil <alex@akeil.net>
2020-12-26 12:49:04 +0100

    Use updated error creation in api module

--- errors.go
+++ errors.go
@@ -2,10 +2,19 @@ package rm
 
 import (
 	"fmt"
+	"net/http"
 )
+
+func Wrap(err error, msg string) error {
+	return fmt.Errorf("%v: %v", msg, err)
+}
 
 type notFound struct {
 	message string
+}
+
+func NewNotFound(s string, v ...interface{}) error {
+	return asNotFound(fmt.Errorf(s, v...))
 }
 
 func (n notFound) Error() string {
@@ -19,4 +28,29 @@ func asNotFound(e error) error {
 func IsNotFound(err error) bool {
 	_, ok := err.(notFound)
 	return ok
+}
+
+func ExpectOK(res *http.Response, msg string) error {
+	return ExpectStatus(res, http.StatusOK, msg)
+}
+
+func ExpectStatus(res *http.Response, expected int, msg string) error {
+	code := res.StatusCode
+
+	if code == expected {
+		return nil
+	}
+
+	if msg != "" {
+		msg = msg + ": "
+	}
+
+	// specific types for selected error codes
+	switch code {
+	case http.StatusNotFound:
+		return NewNotFound("%vgot HTTP status %v", msg, code)
+	}
+
+	// unspecified errors
+	return fmt.Errorf("%vgot HTTP status code %v", msg, code)
 }
