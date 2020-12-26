e282a26a16220fcd1b76c3647dfcab40cdc54add
akeil <alex@akeil.net>
2020-12-26 08:53:08 +0100

    Add documentation for Metadata

--- metadata.go
+++ metadata.go
@@ -14,6 +14,7 @@ type Timestamp struct {
 	time.Time
 }
 
+// NotebookType is used to distinguish betweeen documents and folders.
 type NotebookType int
 
 const (
@@ -21,6 +22,8 @@ const (
 	CollectionType
 )
 
+// Orientation is the layout of a notebook page.
+// It can be Portrait or Landscape.
 type Orientation int
 
 const (
@@ -28,6 +31,7 @@ const (
 	Landscape
 )
 
+// FileType are the different types of supported content for a notebook.
 type FileType int
 
 const (
@@ -38,24 +42,66 @@ const (
 
 // Metadata holds the metadata for a notebook.
 type Metadata struct {
-	Deleted          bool         `json:"deleted"`
-	LastModified     Timestamp    `json:"lastModified"`
-	LastOpenedPage   uint         `json:"lastOpenedPage"`
-	Metadatamodified bool         `json:"metadatamodified"`
-	Modified         bool         `json:"modified"`
-	Parent           string       `json:"parent"`
-	Pinned           bool         `json:"pinned"`
-	Synced           bool         `json:"synced"`
-	Type             NotebookType `json:"type"`
+	// LastModified is the UTC date of the last edit as a Unix timestamp.
+	LastModified Timestamp `json:"lastModified"`
 	// Version is incremented with each change to the file, starting at "1".
 	Version uint `json:"version"`
+	// LastOpenedPage is set by the tablet to the page that was last viewed.
+	LastOpenedPage uint `json:"lastOpenedPage"`
+	// Parent is the ID of the parent folder.
+	// It is empty if the notebook is located in the root folder.
+	// It can also be set to the special value "trash" if the notebook is deleted.
+	Parent string `json:"parent"`
+	// Pinned is the bookmark/start for a notebook.
+	Pinned bool `json:"pinned"`
+	// Type tells whether this is a document or a folder.
+	Type NotebookType `json:"type"`
 	// VisibleName is the display name for this item.
 	VisibleName string `json:"visibleName"`
+	// Deleted seems to be used internally by the tablet(?).
+	Deleted bool `json:"deleted"`
+	// MetadataModified seems to be used internally by the tablet(?).
+	MetadataModified bool `json:"metadatamodified"`
+	// Modified seems to be used internally by the tablet(?).
+	Modified bool `json:"modified"`
+	// Synced seems to be used internally by the tablet(?).
+	Synced bool `json:"synced"`
+}
+
+// Content holds the data from the remarkable `.content` file.
+// It describes the content for a notebook, specifically the sequence of pages.
+// Collections have an empty content object.
+type Content struct {
+	// FileType is the type of content (i.e. handwritten Notebook or PDF, EPUB).
+	FileType FileType `json:"fileType"`
+	// Orientation gives the base layout orientation.
+	// Individual pages can have a different orientation.
+	Orientation Orientation `json:"orientation"`
+	// PageCount is the number of pages in this notebooks.
+	PageCount uint `json:"pageCount"`
+	// Pages is a list of page IDs in the correct order.
+	Pages []string `json:"pages"`
+	// CoverPageNumber is the page that should be used as the cover in the UI.
+	CoverPageNumber int `json:"coverPageNumber"`
+}
+
+// PageMetadata holds the layer information for a single page.
+type PageMetadata struct {
+	// Layers is the list of layers for a page.
+	Layers []LayerMetadata `json:"layers"`
+}
+
+// LayerMetadata describes one layer.
+type LayerMetadata struct {
+	// Name is the display name for this layer.
+	Name string `json:"name"`
+	// TODO: visible y/n?
 }
 
 // ReadMetadata reads a Metadata struct from the given JSON file.
 //
 // Note that you can also use `json.Unmarshal(data, m)`.
+// TODO - remove this?
 func ReadMetadata(path string) (Metadata, error) {
 	var m Metadata
 	data, err := ioutil.ReadFile(path)
@@ -67,9 +113,33 @@ func ReadMetadata(path string) (Metadata, error) {
 	return m, err
 }
 
+// TODO - remove this?
+func ReadContent(path string) (Content, error) {
+	var c Content
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return c, err
+	}
+
+	err = json.Unmarshal(data, &c)
+	return c, err
+}
+
+// TODO - remove this?
+func ReadPageMetadata(path string) (PageMetadata, error) {
+	var p PageMetadata
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return p, err
+	}
+
+	err = json.Unmarshal(data, &p)
+	return p, err
+}
+
 func (t *Timestamp) UnmarshalJSON(b []byte) error {
-	// expects a string lke this: 1607462787637
-	// with the last for digits containing nanoseconds.
+	// Expects a string lke this: "1607462787637",
+	// with the last four digits containing nanoseconds.
 	var s string
 	err := json.Unmarshal(b, &s)
 	if err != nil {
@@ -233,44 +303,4 @@ func (f FileType) String() string {
 	default:
 		return "UNKNOWN"
 	}
-}
-
-// Content holds the data from the remarkable `.content` file.
-// Collections have an empty content object.
-type Content struct {
-	FileType        FileType    `json:"fileType"`
-	Orientation     Orientation `json:"orientation"`
-	PageCount       uint        `json:"pageCount"`
-	Pages           []string    `json:"pages"`
-	CoverPageNumber int         `json:"coverPageNumber"`
-}
-
-func ReadContent(path string) (Content, error) {
-	var c Content
-	data, err := ioutil.ReadFile(path)
-	if err != nil {
-		return c, err
-	}
-
-	err = json.Unmarshal(data, &c)
-	return c, err
-}
-
-type PageMetadata struct {
-	Layers []LayerMetadata `json:"layers"`
-}
-
-type LayerMetadata struct {
-	Name string `json:"name"`
-}
-
-func ReadPageMetadata(path string) (PageMetadata, error) {
-	var p PageMetadata
-	data, err := ioutil.ReadFile(path)
-	if err != nil {
-		return p, err
-	}
-
-	err = json.Unmarshal(data, &p)
-	return p, err
 }
